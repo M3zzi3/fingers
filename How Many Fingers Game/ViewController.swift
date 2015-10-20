180d635c77fb6419445eeefe19006ad94d9b0584
remy <remy@remys-mac.local>
2015-10-20 11:33:45 -0400

    Completed Swift 1.2 version

--- How Many Fingers Game/ViewController.swift
+++ How Many Fingers Game/ViewController.swift
@@ -9,10 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+    @IBOutlet var enteredFingers: UITextField!
+    
+    @IBOutlet var fingersButton: UIButton!
 
+    @IBOutlet var resultLabel: UILabel!
+  
+    @IBAction func fingersCalculator(sender: UIButton) {
+        
+        var fingerGenerator = arc4random_uniform(6)
+        var fingerNumber = enteredFingers.text.toInt()
+        
+        if fingerNumber != nil {
+            
+            if Int(fingerGenerator) == fingerNumber {
+                
+                resultLabel.text = "You are correct"
+                
+            } else {
+                
+                resultLabel.text = "You are wrong it was \(fingerGenerator)"
+            }
+    
+        } else {
+            
+            resultLabel.text = "Please enter a number from 0 to 5"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        fingersButton.setTitle("Guess My Fingers", forState: .Normal)
+        fingersButton.setTitle("Finished Guessing", forState: .Selected)
     }
 
     override func didReceiveMemoryWarning() {
